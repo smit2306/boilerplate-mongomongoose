09514fa0d294a10e677335966c77bfadc0bf0662
Smit Patel <psmit29@gmail.com>
2020-12-05 11:49:01 +0530

    Connecting to mongodb.

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ import * as mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 let Person;
 
 const createAndSavePerson = (done) => {
