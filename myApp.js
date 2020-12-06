3b328951a22cbd771be422e323da8b115f1a9c1b
Smit Patel <psmit29@gmail.com>
2020-12-06 17:02:27 +0530

    Created a schema and a model.

--- myApp.js
+++ myApp.js
@@ -7,7 +7,13 @@ mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true,
 });
 
-let Person;
+let personSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    age: Number,
+    favouriteFoods: [String],
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
     done(null /*, data*/);
